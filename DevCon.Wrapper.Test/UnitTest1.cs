b552e06f02d5a2362ba594573b7fb64a984d5fe2
nvsnkv <nvsnkv@live.com>
2013-08-14 22:09:22 +0400

    initial

---
+++ DevCon.Wrapper.Test/UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace DevCon.Wrapper.Test
+{
+    [TestFixture]
+    public class CommandBuilderShould
+    {
+        protected Configurator Configutator { get; set; }
+
+        [Test]
+        public void CreateProcessStartInfoWithCmdFilePath ()
+        {
+            var cmdPath = Environment.GetEnvironmentVariable("comspec");
+
+            Configutator.Setup(new string[0]);
+            Assert.AreEqual(cmdPath, Configutator.Info.FileName);
+        }
+
+        [Test]
+        public void SetRequireAdministratorFlagIfShortArgumentGiven()
+        {
+            Configutator.Setup(new[] {"-a"});
+            Assert.AreEqual(true, Configutator.IsRequireAdministrator);
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            Configutator = CreateConfigurator();
+        }
+
+        protected virtual Configurator CreateConfigurator()
+        {
+            return new Configurator();
+        }
+    }
+}
